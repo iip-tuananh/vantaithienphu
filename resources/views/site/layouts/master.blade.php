749dd97d072994eea1317d9be11cc142be1bb64d
nguyentienvu4897 <nguyentienvu4897@gmail.com>
2025-11-03 10:32:41 +0700

    vantaithienphu

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -55,8 +55,8 @@
 </head>
 
 <body class="custom-cursor" ng-app="App">
-    {{-- <div class="custom-cursor__cursor"></div>
-      <div class="custom-cursor__cursor-two"></div> --}}
+    <div class="custom-cursor__cursor"></div>
+    <div class="custom-cursor__cursor-two"></div>
     <!--Start Preloader-->
     <div class="loader js-preloader">
         <div></div>
